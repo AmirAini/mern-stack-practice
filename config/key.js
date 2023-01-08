e30516ebd292fc689eb55c54c7780135cb1e352b
AmirAini <amirhaini@gmail.com>
2023-01-08 18:34:59 +0800

    link to docker

---
+++ config/key.js
@@ -0,0 +1,5 @@
+
+//this will export something
+module.exports = {
+    mongoURI:'mongodb://localhost:27017/mern_shopping'
+}
--- server.js
+++ server.js
@@ -1,8 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 //Bodyparser Middleware
 app.use(bodyParser.json());
+
+// DB config (get specific)
+const db = require("./config/key").mongoURI;
+
+// Connect to Mongo (promise)
+mongoose
+  .connect(db)
+  .then(() => console.log("Mongodb connected"))
+  .catch(() => console.log("error"));
+
+
+//run the server
+const port = process.env.port || 5000;
+
+//app to listen to port
+app.listen(port, () => console.log(`connected to port ${port}`));
+
